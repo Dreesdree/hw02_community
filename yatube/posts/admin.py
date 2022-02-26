c95d104d0dfaa0d0370bd0871af769c4660912fe
Денис Иванов <deni_95@list.ru>
2022-02-26 19:45:40 +0500

    Спринт 3. ревью

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -14,6 +14,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('pk',
                     'title',
@@ -23,6 +24,7 @@ class GroupAdmin(admin.ModelAdmin):
     search_fields = ('title',)
     list_filter = ('slug',)
     empty_value_display = '-пусто-'
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
